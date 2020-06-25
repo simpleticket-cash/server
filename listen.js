00d8c8be8d29ae407f60ab003188769f5a24a743
Jonny Heggheim <hegjon@gmail.com>
2020-06-25 19:40:30 +0200

    Use seperate files per HTML document

--- listen.js
+++ listen.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const express = require('express')
+const mustacheExpress = require('mustache-express');
+
 const slpRegister = require('./slp-register');
 
 const { ticket } = require("./config");
@@ -8,96 +10,15 @@ const { ticket } = require("./config");
 function listen(wallet) {
   const app = express();
   app.use(express.json());
-  const port = 3000
-  const html = `
-  <html>
-  <head>
-      <script type="text/javascript" src="https://developer.bitcoin.com/badger/badgerButton-1.0.1.js"></script>
-  </head>
-  <body>
-    <p>Buy ticket to ${ticket.name}</p>
-    <p>Receive address <div id="receive"></div></p>
-    <p>Send Bitcoin cash to address <div id="sendTo">...</div></p>
+  app.set('views', `${__dirname}/views`);
+  app.set('view engine', 'mustache');
+  app.engine('mustache', mustacheExpress());
 
-    <p>Pay for ticket</p>
-    <p>
-      <button id="badger-button">
-        Pay 1000 Satoshis to buy a ticket!
-      </button>
-    </p>
+  const port = 3000;
 
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=Array.from%2CPromise%2CSymbol%2CObject.setPrototypeOf%2CObject.getOwnPropertySymbols"></script>
-    <script src="https://cdn.jsdelivr.net/npm/superagent"></script>
-
-      <script>
-        var badgerButton = document.getElementById("badger-button")
-
-        console.log("HEI");
-
-        var a = new Web4Bch(web4bch.currentProvider)
-
-        setTimeout(() => {
-          const wallet = new Web4Bch(web4bch.currentProvider)
-          console.log("REC: ", wallet.bch.defaultSlpAccount);
-          document.getElementById('receive').innerHTML = wallet.bch.defaultSlpAccount;
-        }, 100);
-
-        function sendTo(address) {
-          var txParams = {
-            to: address,
-            from: web4bch.bch.defaultAccount,
-            value: "1000"
-          }
-          web4bch.bch.sendTransaction(txParams, (err, res) => {
-            if (err) {
-              console.log('send err', err)
-            } else {
-              console.log('send success, transaction id:', res)
-
-              superagent
-                .post('/watch')
-                .send(txParams)
-                .end((err, res) => {
-                  if(err) {
-                    return cosole.log("ERR: ", err);
-                  }
-                  const address = res.text;
-                  console.log("RES: ", address);
-                  document.getElementById('sendTo').innerHTML = address;
-                  sendTo(address);
-                });
-
-            }
-          })
-        }
-
-        badgerButton.addEventListener('click', function(event) {
-          if (typeof web4bch !== 'undefined') {
-            web4bch = new Web4Bch(web4bch.currentProvider)
-
-            superagent
-              .post('/register')
-              .send({ slpReceiveAddress: web4bch.bch.defaultSlpAccount})
-              .end((err, res) => {
-                if(err) {
-                  return cosole.log("ERR: ", err);
-                }
-                const address = res.text;
-                console.log("RES: ", address);
-                document.getElementById('sendTo').innerHTML = address;
-                sendTo(address);
-              });
-          } else {
-            console.log("Ensure Badger Wallet is installed and unlocked, then refresh this page")
-          }
-        })
-      </script>
-
-  </body>
-  </html>
-  `
-
-  app.get('/', (req, res) => res.send(html))
+  app.get('/', (req, res) => {
+    res.render('index', { ticket: ticket })
+  });
 
   app.post('/register', (req, res) => {
     console.log("REQ :%j", req.body);
